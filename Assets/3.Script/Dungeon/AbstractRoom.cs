9fa75a1b36e9751746e756e5298273c1cb978291
gyusummer <gyusummer@gmail.com>
2025-02-25 18:02:54 +0900

    dungeon path processing

--- Assets/3.Script/Dungeon/AbstractRoom.cs
+++ Assets/3.Script/Dungeon/AbstractRoom.cs
@@ -1,8 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AbstractRoom : MonoBehaviour
+public class AbstractRoom
 {
-    
+    public List<AbstractRoom> reachableRoom;
+
+    public void AddPath(AbstractRoom room)
+    {
+        reachableRoom.Add(room);
+    }
 }
--- Assets/3.Script/Dungeon/DungeonMaster.cs
+++ Assets/3.Script/Dungeon/DungeonMaster.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class DungeonMaster : MonoBehaviour
 {
     public static DungeonMaster Instance = null;
-    AbstractRoom[,] dungeon;
+    Dungeon dungeon;
+    int floorMax = 15;
+    int roomPerFloor = 7;
+    int pathsMax = 6;
+    
     private void Awake()
     {
         if(Instance == null)
@@ -19,11 +23,60 @@ public class DungeonMaster : MonoBehaviour
     }
     public void GenerateDungeon()
     {
-        dungeon = new AbstractRoom[7, 15];
-        int numofStartpoint = Random.Range(2, 5);
-        for (int i = 0; i <= numofStartpoint; i++)
+        dungeon = new Dungeon(roomPerFloor,floorMax);
+        for(int count = 0; count < pathsMax; count++)
         {
-
+            int startPoint = Random.Range(0, 7);
+            int[] path = MakeAPath(startPoint);
         }
     }
+    int[] MakeAPath(int startPoint)
+    {
+        int[] path = new int[floorMax];
+        path[0] = startPoint;
+        dungeon.AddRoom(startPoint, 0, new AbstractRoom());
+        for(int floor = 1; floor < path.Length; floor++)
+        {
+            int curRoom = path[floor - 1];
+            int minRoom = curRoom == 0 ? 0 : curRoom - 1;
+            int maxRoom = curRoom == 6 ? 6 : curRoom + 1;
+            path[floor] = Random.Range(minRoom, maxRoom + 1);
+        }
+        return path;
+    }
+
+
+    #region Test
+    List<int[]> testPath_list = new List<int[]>();
+    int[] testPath;
+    public void Test()
+    {
+        testPath = MakeAPath(Random.Range(0,7));
+        string PathStr = "";
+        foreach(int roomNum in testPath)
+        {
+            PathStr = PathStr + " " + roomNum;
+        }
+        Debug.Log(PathStr);
+        testPath_list.Add(testPath);
+    }
+    private void OnDrawGizmos()
+    {
+        if(testPath_list.Count > 0)
+        {
+            foreach(int[] path in testPath_list)
+            {
+                Gizmos.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+                for (int y = 1; y < path.Length; y++)
+                {
+                    int x = path[y];
+                    int beforeX = path[y - 1];
+                    Vector2 curPoint = new Vector2(x, y);
+                    Vector2 beforePoint = new Vector2(beforeX, y - 1);
+                    Gizmos.DrawLine(beforePoint, curPoint);
+                }
+            }
+        }
+    }
+    #endregion
 }
