4bd1f3a8999e3b0bc6070917eeb8d0e64529eb3c
gyusummer <gyusummer@gmail.com>
2025-02-25 14:12:06 +0900

    DOTween install, DungeonMaster singleton

--- Assets/3.Script/Dungeon/DungeonMaster.cs
+++ Assets/3.Script/Dungeon/DungeonMaster.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class DungeonMaster : MonoBehaviour
 {
-    
+    public static DungeonMaster Instance = null;
+    AbstractRoom[,] dungeon;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     public void GenerateDungeon()
     {
+        dungeon = new AbstractRoom[7, 15];
+        int numofStartpoint = Random.Range(2, 5);
+        for (int i = 0; i <= numofStartpoint; i++)
+        {
 
+        }
     }
 }
--- Assets/3.Script/UI/TitleMenu.cs
+++ Assets/3.Script/UI/TitleMenu.cs
@@ -17,6 +17,7 @@ public class TitleMenu : MonoBehaviour
 
     public void Quit()
     {
+        Debug.Log("quit");
         Application.Quit();
     }
 }
