5d9d915abf9c5fe18cb561cde91bb7d28456e636
gyusummer <gyusummer@gmail.com>
2025-02-27 17:36:19 +0900

    .

--- Assets/3.Script/Data/ScriptableObject/Relic.cs
+++ Assets/3.Script/Data/ScriptableObject/Relic.cs
@@ -2,7 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Relic : ScriptableObject
+namespace ScriptableObjects
 {
+    [Serializable]
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Artifact", order = 1)]
+    public class Relic : ScriptableObject
+    {
     
+    }
 }
